bd8e1d4b7c42c8f123f3f8c61c99eebd5abaf440
MrZengCHN <90165753+MrZengCHN@users.noreply.github.com>
2025-11-27 23:27:21 +0800

    修改build路径

--- blog/vite.config.js
+++ blog/vite.config.js
@@ -1,4 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,7 +10,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Blog/',
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+  },
   plugins: [
+    {
+      name: 'clean-assets',
+      buildStart() {
+        const assetsDir = path.resolve(__dirname, '../assets')
+        if (fs.existsSync(assetsDir)) {
+          fs.rmSync(assetsDir, { recursive: true, force: true })
+          console.log(`\nDeleted ${assetsDir}`)
+        }
+      }
+    },
     vue(),
     vueDevTools(),
     tailwindcss(),
